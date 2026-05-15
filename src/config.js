c3db07117fd3db544e3b33d06d2337d7334e8aa3
Masterbe99-spec <gabriel.jean.pierre00@gmail.com>
2026-05-15 17:44:49 +0200

    Add wedding configuration file

---
+++ src/config.js
@@ -0,0 +1,18 @@
+// Wedding Configuration - Update these values as needed
+export const WEDDING_CONFIG = {
+  couple: {
+    name1: 'Jeannette',
+    name2: 'Georges'
+  },
+  // Wedding date and time - CENTRAL CONFIG, update here to change everywhere
+  date: {
+    day: 'Saturday',
+    date: 'June 27, 2026',
+    time: '6:00 PM' // Update the time here as needed
+  },
+  emails: {
+    main: 'Khorkhehachem@gmail.com',
+    cc: 'jeannettebouenek@gmail.com'
+  },
+  formAction: 'https://formsubmit.co/Khorkhehachem@gmail.com'
+}
